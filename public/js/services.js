34356b98ab30c9ee06e57cbc3e0defa60e7f08d4
Chris Searle <chris@chrissearle.org>
2014-11-12 13:21:21 +0100

    Re-add incorrectly deleted file

---
+++ public/js/services.js
@@ -0,0 +1,18 @@
+/* global angular */
+
+var expensesApp = angular.module("expensesApp");
+
+expensesApp.factory("DataStore", [function () {
+    "use strict";
+
+    return {
+        rows: function() {
+            return this.data.items.filter(function (candidate) {
+                return candidate.date && candidate.description && candidate.cost;
+            });
+        },
+        data: {
+            items: []
+        }
+    };
+}]);
